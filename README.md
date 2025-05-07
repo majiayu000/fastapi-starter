3e78ce639daf3e45b739eeca7b08c5ec33b373e8
lif <1835304752@qq.com>
2025-05-07 10:52:21 +0800

    Add usage instructions to README, clean up main.py, and initialize pyproject.toml and poetry.lock
    - Updated README.md to include usage instructions for running the FastAPI application.
    - Removed unused imports and commented-out code from main.py for better readability.
    - Created pyproject.toml to define project metadata and dependencies.
    - Added poetry.lock to lock dependencies for consistent installations.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # fastapi-starter
 A starter project for fastApi
+
+# Usage
+`poetry run uvicorn main:app --host 0.0.0.0 --port 8000 --workers 4 --proxy-headers`
--- main.py
+++ main.py
@@ -1,19 +1,7 @@
 from fastapi import FastAPI, HTTPException, Depends, status
-from sqlalchemy import create_engine
-import pandas as pd
-from sqlalchemy.exc import SQLAlchemyError
-from pydantic import BaseModel
-from datetime import datetime
-from sqlalchemy import create_engine, Column, Integer, String, DateTime
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.sql import func
-from db import SessionLocal, engine
-import crud, models, schemas
-from sqlalchemy.orm import Session
 from api import api_router
 from fastapi.middleware.cors import CORSMiddleware
-from worker import celery_app, add
+
 
 # models.Base.metadata.create_all(bind=engine)
 # 创建 FastAPI 实例
@@ -32,12 +20,6 @@ app.add_middleware(
 @app.get("/hi")
 async def read_data():
     return "hello world"
-
-
-@app.get("/task")
-async def process_endpoint():
-    result = add.delay(1, 2)
-    return {"task_id": result.id}
 
 
 app.include_router(api_router)
